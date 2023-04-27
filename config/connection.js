a9e43a23be511b4d0e1474c001e9ded16d074a6e
CF <cfreitas12345@gmail.com>
2023-04-27 14:00:21 -0700

    Completed the set-up for files and folders.

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
+require("dotenv").config();
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/mygroceryDB');
+mongoose.connect(process.env.MONGO_URL);
 
 // Export connection 
 module.exports = mongoose.connection;
